68d68d2f2ba7d57903d669253c104e7e990e8fdf
ahmed khaled <ahmedelads789@gmail.com>
2024-01-29 12:12:21 +0200

    Show BottomModelSheet

---
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddBottomModelSheet extends StatelessWidget {
+  const AddBottomModelSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
